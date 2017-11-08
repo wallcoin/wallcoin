8b3458d4926757da605e08932525a6bab50e8a2f
wallocin <wallcoin.co@gmail.com>
2017-11-08 07:31:29 +0700

    a

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define WALLCOIN-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "WALLCOINURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
